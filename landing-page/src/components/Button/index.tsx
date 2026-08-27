e90a8af3a80c2e072c6cb2a4b98b7ce1f8b3676c
caioaroliveira-gif <caio_ar_oliveira@estudante.sesisenai.org.br>
2026-08-26 21:57:36 -0300

    feat: Adiconando Cards, e melhorando components.

---
+++ landing-page/src/components/Button/index.tsx
@@ -0,0 +1,18 @@
+interface Buttons {
+        text: string;
+        color: string
+        text_color:string
+        backgroud_color:string;
+}
+
+
+export default function Button({text, color, text_color, backgroud_color}: Buttons){
+
+    return (
+        <>
+        <div>
+            
+        </div>
+        </>
+    )
+}
